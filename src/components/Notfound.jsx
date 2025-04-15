f3a0b9c4f4687e9c8c940a75c27e9147e0d02733
Sairaj182 <sairajraithatha.official@gmail.com>
2025-04-15 19:51:15 +0530

    trying to solve vercel problem

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Notfound = () => {
   return (
     <>
-      
+      Kindly Login first on TradeSathi: <a href="https://paper-trading-hosting.vercel.app/login">TradeSathi Login</a>
     </>
 
   )
